29236314efe5b77a2e53995d79a633e2e32a098e
Angel Valdivia <angelv1901@gmail.com>
2019-05-21 14:59:58 -0400

    update

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,25 @@
+
+
+
 "use strict"
 
-const getMoreQuotesButton = document.getElementById('getMoreQuotes');
-getMoreQuotesButton.addEventListener('click', function(e){
+const getCharactersButton = document.getElementById('getCharacters');
+getCharactersButton.addEventListener('click', function(e){
     e.preventDefault();
-    updateChuckSays();
+    updateCharacterList();
 });
 
-function updateChuckSays() {
-    const chuckSays = document.getElementById('chuckSays');
+function updateCharacterList() {
+    const characterList = document.getElementById('characterList');
 
     get('https://rickandmortyapi.com/api/character')
+    // gets end point to get character from api. end point has list of info 
+    // that we capture.
     .then((response) => {
-        console.log(response.results);
+        // .then is the promise /the response is what the api gives you back
+        // => is another form of a function
+        // function (response) other way it could be writen
+        console.log(response.results)
         // take ALL the results, and forEach() result, 
         // add an LI to the main list...
         // 
@@ -19,15 +27,30 @@ function updateChuckSays() {
         //    doStuff()
         // }
         const characters = response.results;
+        // response results are equal to the variable of charcaters
+        // in this the case the list of characters
         characters.forEach(function(character){
-            console.log(character);
+            // we going through each character on the characters list
+        
 
             let characterItem = document.createElement('li');
+            // here we are creating a list item element to characterItem
             let locationLink = document.createElement('a');
+            // here we are creating an anchor element to the location link
             locationLink.setAttribute('href',character.location.url);
+            // the attribute is the href and the value is character.location.url
             locationLink.textContent = character.name;
+            // character.name is being set locationLink or element 'a'
+            // the method .textContent set it to objects name
+            // if method .textContent is deleted you have no anchor 'a' element
+            
+
             characterItem.append(locationLink);
-            chuckSays.append(characterItem);
+            // .append is another javascript method in the case the locationLink
+            // is being appended to the characterItem
+            characterList.append(characterItem);
+            // here characterItem is being appended to the 'ul' characterList 
+            // 
 
 
         })
@@ -41,11 +64,13 @@ document.addEventListener('click', function(e){
 })
 
 function getCharacterLocation(character){
-    console.log(character.href);
+
     get(character.href)
-    .then(response=>{
+    // this line is hitting the second end point for location
+    .then(response => {
+        console.log(response)
         const locationName = document.createElement("p");
-        locationName.textContent=response.name;
+        locationName.textContent = response.name;
         character.parentNode.append(locationName);
 
     })
